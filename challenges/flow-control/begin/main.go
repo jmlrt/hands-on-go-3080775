47591e970fb6eedf95f40e56a31b4c252cee0ac3
Julien Mailleret <8582351+jmlrt@users.noreply.github.com>
2023-03-28 14:43:06 +0000

    add challenges

--- challenges/flow-control/begin/main.go
+++ challenges/flow-control/begin/main.go
@@ -1,28 +1,62 @@
 // challenges/flow-control/begin/main.go
 package main
 
+import (
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
 func main() {
+
 	// handle any panics that might occur anywhere in this function
-	//
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println("recovered from panic:", err)
+		}
+	}()
 
 	// use os package to read the file specified as a command line argument
-	//
+	fileName := os.Args[1]
+	//fmt.Println(fileName)
+	content, err := os.ReadFile(fileName)
 
 	// convert the bytes to a string
-	//
+	var text string
+	if err != nil {
+		panic(err)
+	} else {
+		text = string(content)
+	}
 
 	// initialize a map to store the counts
-	//
+	counts := make(map[string]int)
 
 	// use the standard library utility package that can help us split the string into words
-	//
+	words := strings.Fields(text)
 
 	// capture the length of the words slice
-	//
+	counts["words"] = len(words)
 
 	// use for-range to iterate over the words slice and for each word, count the number of letters, numbers, and symbols, storing them in the map
-	//
+	for _, word := range words {
+		for _, character := range word {
+			if unicode.IsLetter(character) {
+				counts["letters"]++
+			}
+			if unicode.IsSymbol(character) {
+				counts["symbols"]++
+			}
+			if unicode.IsNumber(character) {
+				counts["numbers"]++
+			}
+		}
+
+	}
 
 	// dump the map to the console using the spew package
-	//
+	spew.Dump(counts)
 }
--- challenges/types-composite/begin/main.go
+++ challenges/types-composite/begin/main.go
@@ -1,38 +1,61 @@
 // challenges/types-composite/begin/main.go
 package main
 
+import "fmt"
+
 // define an author type with a name
-//
+type author struct {
+	name string
+}
 
 // define a book type with a title and author
-//
+type book struct {
+	title string
+	author author
+}
 
 // define a library type to track a list of books
-//
+// type library map[string][]book
+type library struct {
+	books []book
+}
 
 // define addBook to add a book to the library
-//
+func (l *library) addBook(book book) {
+	l.books = append(l.books, book)
+}
 
 // define a lookupByAuthor function to find books by an author's name
-//
+func (l *library) lookupByAuthor(authorName string) []book {
+	books := make([]book, 0)
+	for _, b := range(l.books) {
+		if b.author.name == authorName {
+			books = append(books, b)
+		}
+	}
+	return books
+}
 
 func main() {
 	// create a new library
-	//
+	myLibrary := library{}
 
 	// add 2 books to the library by the same author
-	//
+	myLibrary.addBook(book{"20,000 Leagues Under the Sea", author{"Jules Verne"}})
+	myLibrary.addBook(book{"Around the World in 80 Days", author{"Jules Verne"}})
 
 	// add 1 book to the library by a different author
-	//
+	myLibrary.addBook(book{"Gulliver's Travels", author{"Jonathan Swift"}})
 
 	// dump the library with spew
-	//
+	fmt.Printf("%v\n", myLibrary)
 
 	// lookup books by known author in the library
-	//
+	lookupBooks := myLibrary.lookupByAuthor("Jules Verne")
+	fmt.Printf("%v\n", lookupBooks)
 
 	// print out the first book's title and its author's name
-	//
+	firstBook := lookupBooks[0]
+	fmt.Printf("%s - %s\n", firstBook.title, firstBook.author.name)
 
 }
